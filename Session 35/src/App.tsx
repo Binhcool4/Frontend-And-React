fa4f46f07e38f578b1e6f9178c0ff7115f2c5c23
Binhcool4 <binhtrieuhn99@gmail.com>
2025-09-24 21:01:00 +0700

    ss35

--- Session 35/src/App.tsx
+++ Session 35/src/App.tsx
@@ -1,21 +1,21 @@
-import Counter from "./components/Counter";
-import RandomNumber from "./components/RandomNumber";
-import ThemeSwitcher from "./components/ThemeSwitcher";
-import DataView from "./components/DataView";
-import SideMenu from "./components/SideMenu";
-import LanguageSwitcher from "./components/LanguageSwitcher";
-import UsersList from "./components/UserList";
+// import Counter from "./components/Counter";
+// import RandomNumber from "./components/RandomNumber";
+// import ThemeSwitcher from "./components/ThemeSwitcher";
+// import DataView from "./components/DataView";
+// import SideMenu from "./components/SideMenu";
+// import LanguageSwitcher from "./components/LanguageSwitcher";
+// import UsersList from "./components/UserList";
 
 function App() {
   return (
-    <div style={{ fontFamily: "Arial, sans-serif", padding: "20px" }}>
-      <SideMenu />
-      <LanguageSwitcher />
-      <ThemeSwitcher />
-      <DataView />
-      <Counter />
-      <RandomNumber />
-      <UsersList />
+    <div>
+      {/* <SideMenu /> */}
+      {/* <LanguageSwitcher /> */}
+      {/* <ThemeSwitcher /> */}
+      {/* <DataView /> */}
+      {/* <Counter /> */}
+      {/* <RandomNumber /> */}
+      {/* <UsersList /> */}
     </div>
   );
 }
